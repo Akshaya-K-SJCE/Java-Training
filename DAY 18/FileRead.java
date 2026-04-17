78dfbeeea1ef0762aa91fab534a7d12646f814a9
Akshaya <akshayakannan09101@gmail.com>
2026-04-17 09:02:29 +0530

    Add DAY18 folder

---
+++ DAY 18/FileRead.java
@@ -0,0 +1,22 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
+    public static void main(String[] args) {
+        {
+            try{
+                FileReader fr=new FileReader("test.txt");
+                int character;
+                while((character=fr.read())!=-1)
+                {
+                    System.out.print((char)character);
+                }
+            }
+            catch(IOException e)
+            {
+                System.out.println(e);
+            }
+            
+        }
+    }
+}
